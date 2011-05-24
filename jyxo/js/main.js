cc0d7a96ce8e6adcc484d1517c7a74705cd0a3e3
Jaroslav Hanslik <kukulich@kukulich.cz>
2011-05-24 13:43:17 +0200

    Template and config for TR ApiGen 2.0 beta 3

--- jyxo/js/main.js
+++ jyxo/js/main.js
@@ -1,13 +1,16 @@
 $(function(){
 
 	// enables search autocompletion
-	$("#search input[type=text]").autocomplete(classes, {
-		matchContains: true,
-		scrollHeight: 200,
-		max: 20,
-		formatItem: function(row) { return row[0].replace(/^(.+\\)(.+)$/, '<small>$1</small>$2'); },
-		formatMatch: function(row) { return row[0]; }
-	});
+	var $search = $("#search input[type=text]");
+	if ($search.size()) {
+		$search.autocomplete(elements, {
+			matchContains: true,
+			scrollHeight: 200,
+			max: 20,
+			formatItem: function(row) { return row[0].replace(/^(.+\\)(.+)$/, '<small>$1</small>$2'); },
+			formatMatch: function(row) { return row[0]; }
+		});
+	}
 
 	// saves original order
 	$("table.summary:has(tr[data-order]) tr").each(function(index) {
@@ -32,29 +35,33 @@ $(function(){
 		$("table.summary:has(tr[data-order]) caption").click();
 	}
 
-	// delayed hover efect on method summary
+	// delayed hover efect on summary
 	var timeout;
 	$("tr:has(.detailed)").hover(function(){
 		clearTimeout(timeout);
-		var $tr = $(this);
+		var $this = $(this);
 		timeout = setTimeout(function(){
-			$tr.find('.short').hide();
-			$tr.find('.detailed').show();
+			$this.find('.short').hide();
+			$this.find('.detailed').show();
 		}, 500);
 	}, function(){
 		clearTimeout(timeout);
 
-	}).click(function(){ // immediate hover effect on method summary
+	}).click(function(){ // immediate hover effect on summary
 		clearTimeout(timeout);
-		$(this).find('.short').hide();
-		$(this).find('.detailed').show();
+		var $this = $(this);
+		$this.find('.short').hide();
+		$this.find('.detailed').show();
 	});
 
-	// hide deep namespaces
-	$('#left ul li ul li:not(.active):not(:has(.active)) ul').addClass('collapsed');
-	$('#left > ul > li > ul > li').hover(function() {
-		$('ul.collapsed', this).stop(true, true).delay(300).slideDown();
+	// hide deep packages and namespaces
+	$('#left ul span').click(function() {
+		$(this)
+			.toggleClass('collapsed')
+			.next('ul')
+			.toggleClass('collapsed');
 	});
+	$('#left ul li ul li:not(.active) span').click();
 
 	// splitter
 	$('#main').splitter({
